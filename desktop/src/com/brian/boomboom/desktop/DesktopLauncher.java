0a81da11b7d35989f55fb9f2e906a03609246840
Brian <bp2008@users.noreply.github.com>
2017-12-27 08:42:33 -0700

    Empty Project Buildable

---
+++ desktop/src/com/brian/boomboom/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.brian.boomboom.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.brian.boomboom.BoomBoomGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BoomBoomGame(), config);
+	}
+}
